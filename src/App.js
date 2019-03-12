9b33c2a950a6f69fb045fbe31059628e6553adf5
phamin2001 <amin.pahlavani@gmail.com>
2019-03-11 19:41:55 -0600

    error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainContainer from './MainContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      test
+        <MainContainer />
       </div>
-    );
+    )
   }
 }
 
---
+++ src/MainContainer/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class MainContainer extends Component {
+
+
+    returrn(){
+
+        return(
+            <div>
+                <h1>I'm MainContainer</h1>
+            </div>
+        )
+    }
+}
+
+export default MainContainer;
